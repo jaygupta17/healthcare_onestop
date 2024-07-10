4397aeeb3f6434e728e011a61567da6654c91acb
OmChillure <omchillure4@gmail.com>
2024-07-10 08:04:05 +0530

    push

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { motion } from "framer-motion";
 import React from "react";
-import { ImagesSlider } from "../components/ui/image-slider";
+import { ImagesSlider } from "./ui/image-slider";
 
 export function ImagesSliderDemo() {
   const images = [
